2451ced86fe173b182661a64f1cc67eebabcaf01
mythos <kuangcp@aliyun.com>
2018-07-16 00:36:52 +0800

    调整count

--- count/count.go
+++ count/count.go
@@ -12,18 +12,29 @@ import (
 // TODO 分析用字的数据, 得到一个统计报表
 
 var totalFile int = 0
+var wordDetail bool = false
+var printf = fmt.Printf
+var println = fmt.Println
 
-func walkfunc(path string, info os.FileInfo, err error) error {
+// 往递归遍历目录 作为参数传入的函数 
+func handlerDir(path string, info os.FileInfo, err error) error {
 	if(info.IsDir()){
 		return nil
 	}
-	if(strings.Contains(path, ".git")){
-		return nil
+	var ignoreList = [...]string{
+		".md",
+		".markdown", 
+		".txt", 
 	}
-	// fmt.Println(path)
-	readfile(path)
+	for _, fileType := range ignoreList {
+		if strings.HasSuffix(path, fileType) {
+			readfile(path)
+			return nil
+		}
+	}
     return nil
 }
+// 根据文件路径读取文件字节
 func readfile(fileName string){
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -37,13 +48,15 @@ func readfile(fileName string){
 		fmt.Println("ioutil ReadAll error: ", err)
 		return
 	}
-	var total int = showchar(content)
-	// fmt.Println("\n ", fileName, " total char : ", total)
-	// fmt.Println(fileName, " -> chinese char : ", total)
-	fmt.Printf("%-60v -> chinese char = \033[0;32m %v \033[0m \n", fileName, total)
+	var total int = countChineseChar(content)
+	// printf("%-50v-> chinese char =\033[0;32m %v\033[0m \n", fileName, total)
+	if !wordDetail{
+		printf(">\033[0;32m %-5v\033[0m %v\n", total, fileName)
+	}
 	totalFile += total
 }
-func showchar(origin []byte) int{
+// 计算中文字符
+func countChineseChar(origin []byte) int{
 	var total int = 0
 	temp := [3]byte{}
 	var count int = 0
@@ -60,16 +73,47 @@ func showchar(origin []byte) int{
 		}
 		if count == 3 {
 			// fmt.Println("ch char : ", temp, string(temp[0:3]))
-			// fmt.Printf(string(temp[0:3]))
+			if wordDetail {
+				printf(string(temp[0:3]))
+			}
 			total ++
 			count = 0
 		}
 	}
 	return total
 }
+// TODO 分析 所有汉字
+func analysisTotalChar(){
+
+}
+// 读取参数 
+func handlerArgs(verb string, param string){
+	switch verb {
+		case "-h":
+			var format string = "%-5v %-10v %v \n"
+			printf(format, "-h", "", "帮助")
+			printf(format, "-w", "", "输出所有汉字")
+			os.Exit(0)
+		case "-w":
+			wordDetail = true
+			break
+	}
+}
 
 func main() {
-	filepath.Walk("./", walkfunc)
-	fmt.Println()
-	fmt.Println("all file chinese char : ", totalFile)
+	// 递归遍历目录
+	var argLen int = len(os.Args)
+	// printf("len of args:%d\n", argLen)
+	// for i, v := range os.Args {
+	// 	printf("args[%d]=%s\n", i, v)
+	// }
+
+	if argLen == 2 {
+		handlerArgs(os.Args[1], "")
+	}
+	if argLen == 3 {
+		handlerArgs(os.Args[1], os.Args[2])
+	}
+	filepath.Walk("./", handlerDir)
+	printf("\n\033[0;35m Total characters of all files : \033[0;33m %v \033[0m \n", totalFile)
 }
