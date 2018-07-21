308b696420a5307c7ed4dff4e9e5921d348f350d
mythos <kuangcp@aliyun.com>
2018-07-21 19:42:49 +0800

    optimize to simple

--- translation/translation.go
+++ translation/translation.go
@@ -11,18 +11,8 @@ import(
 var printf = fmt.Printf
 var println = fmt.Println
 
-func handlerArgs(verb string, param []string){
-	switch verb {
-		case "-h":
-			var format string = "\033[0;32m %-5v \033[0;33m%-10v \033[0m%v \n"
-			printf(format, "-h", "", "帮助")
-			printf(format, "-i", "ze/ez", "使用爱词霸翻译 zh-en/en-zh")
-			os.Exit(0)
-		case "y":
-			// 单词或者一句话 "how are you "
-			sendYoudaoEntoZh(param[0])
-			break
-	}
+func handlerArgs(verb string){
+	sendYoudaoEntoZh(verb)
 }
 func sendGet(url string)[]byte{
 	resp, err := http.Get(url)
@@ -44,7 +34,6 @@ func readConfig() string{
     }
 	return string(b)
 }
-// TODO 将本地化数据 放在用户目录下
 // TODO 同样的参数做正则检验
 // https://ai.youdao.com/docs/doc-trans-api.s#p01
 func sendYoudaoEntoZh(word string){
@@ -71,10 +60,9 @@ func sendYoudaoEntoZh(word string){
 
 func main(){
 	var argLen int = len(os.Args)
-	if argLen == 2 {
-		handlerArgs(os.Args[1], nil)
+	if argLen < 2 {
+		println("Please input word")
+		return
 	}
-	if argLen > 2 {
-		handlerArgs(os.Args[1], os.Args[2:])
-	}
+	handlerArgs(os.Args[1])
 }
