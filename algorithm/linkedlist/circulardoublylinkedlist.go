85ed2127c8ddefb3a9a9c79d250ce369e23253e2
Kuangcp <kuangcp@aliyun.com>
2019-10-25 00:41:03 +0800

    +) time wheel

---
+++ algorithm/linkedlist/circulardoublylinkedlist.go
@@ -0,0 +1,8 @@
+package linkedlist
+
+type DoublyLinkedNode struct {
+	pre *DoublyLinkedNode
+	// how to generic
+	element int
+	next *DoublyLinkedNode
+}
---
+++ algorithm/timewheel/timewheel.go
@@ -0,0 +1,8 @@
+package timewheel
+
+
+// https://juejin.im/post/5b5e52ecf265da0f716c3203
+// https://github.com/wuYin/timewheel
+// https://my.oschina.net/anur/blog/2252539
+
+
