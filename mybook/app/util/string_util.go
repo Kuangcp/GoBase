5acec8c3088e5bfc4f01cddb3a1c364464cf052e
kcp <kuangcp@aliyun.com>
2020-04-09 21:13:19 +0800

    *) format

--- mybook/app/util/string_util.go
+++ mybook/app/util/string_util.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 )
 
+// 中文字符占用 1.5 英文字符宽度
 func BuildCHCharFormat(expectLen int, str string) string {
 	return "%" + strconv.Itoa(expectLen-len(str)/3*2) + "s"
 }
@@ -12,7 +13,7 @@ func BuildCHCharFormat(expectLen int, str string) string {
 func Json(data interface{}) string {
 	bytes, e := json.Marshal(data)
 	if e != nil {
-		return " ERROR"
+		return "ERROR"
 	}
 	return string(bytes)
 }
