a9a492b06bd5fc6b268f776be58fd9fb5f4628e3
Kuangcp <kuangcp@aliyun.com>
2020-04-25 23:10:59 +0800

    +) home, readLines

--- cuibase/cuibase.go
+++ cuibase/cuibase.go
@@ -1,10 +1,18 @@
 package cuibase
 
 import (
+	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"os/exec"
+	"os/user"
+	"runtime"
 	"strconv"
+	"strings"
 )
 
 // Color
@@ -85,6 +93,44 @@ func CheckIfError(err error) {
 	os.Exit(1)
 }
 
+func ReadFileLines(filename string, filterFunc func(string) bool, mapFunc func(string) interface{}) []interface{} {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0666)
+	if err != nil {
+		log.Println("Open file error!", err)
+		return nil
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+	if stat.Size() == 0 {
+		log.Printf("file:%s is empty", filename)
+		return nil
+	}
+
+	var result []interface{}
+
+	buf := bufio.NewReader(file)
+	for {
+		line, err := buf.ReadString('\n')
+		if filterFunc(line) {
+			result = append(result, mapFunc(line))
+		}
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Println("Read file error!", err)
+				return nil
+			}
+		}
+	}
+	return result
+}
+
 func runAction(params []string, actions map[string]func(params []string), defaultAction func(params []string)) {
 	if len(params) < 2 {
 		defaultAction(os.Args)
@@ -98,6 +144,58 @@ func runAction(params []string, actions map[string]func(params []string), defaul
 	} else {
 		action(os.Args)
 	}
+}
+
+func Home() (string, error) {
+	curUser, err := user.Current()
+	if nil == err {
+		return curUser.HomeDir, nil
+	}
+
+	// cross compile support
+
+	if "windows" == runtime.GOOS {
+		return homeWindows()
+	}
+
+	// Unix-like system, so just assume Unix
+	return homeUnix()
+}
+
+func homeUnix() (string, error) {
+	// First prefer the HOME environmental variable
+	if home := os.Getenv("HOME"); home != "" {
+		return home, nil
+	}
+
+	// If that fails, try the shell
+	var stdout bytes.Buffer
+	cmd := exec.Command("sh", "-c", "eval echo ~$USER")
+	cmd.Stdout = &stdout
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	result := strings.TrimSpace(stdout.String())
+	if result == "" {
+		return "", errors.New("blank output when reading home directory")
+	}
+
+	return result, nil
+}
+
+func homeWindows() (string, error) {
+	drive := os.Getenv("HOMEDRIVE")
+	path := os.Getenv("HOMEPATH")
+	home := drive + path
+	if drive == "" || path == "" {
+		home = os.Getenv("USERPROFILE")
+	}
+	if home == "" {
+		return "", errors.New("HOMEDRIVE, HOMEPATH, and USERPROFILE are blank")
+	}
+
+	return home, nil
 }
 
 func enoughCount(params []string, count int) bool {
--- toolbox/repo-man/main.go
+++ toolbox/repo-man/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-git/go-git/v5"
 	"github.com/kuangcp/gobase/cuibase"
 	"github.com/wonderivan/logger"
+	"strings"
+	"sync"
 )
+
+type RepoAlias struct {
+	alias string
+	path  string
+	name  string
+}
+
+func (this RepoAlias) String() string {
+	return fmt.Sprintf("%v%-30s %v%-50s %v%-10v%v",
+		cuibase.Yellow, this.alias,
+		cuibase.Green, this.path,
+		cuibase.Blue, this.name, cuibase.End)
+}
 
 func HelpInfo(_ []string) {
 	info := cuibase.HelpInfo{
@@ -19,10 +36,93 @@ func HelpInfo(_ []string) {
 		}}
 	cuibase.Help(info)
 }
+func PullRepo(dir string, latch *sync.WaitGroup) {
+	defer latch.Done()
+
+	r, err := git.PlainOpen(dir)
+	cuibase.CheckIfError(err)
+	worktree, err := r.Worktree()
+	cuibase.CheckIfError(err)
+
+	// TODO ssh bug
+	err = worktree.Pull(&git.PullOptions{SingleBranch: true})
+	logger.Error(dir, err)
+}
+
+func ShowRepoStatus(dir string, latch *sync.WaitGroup) {
+	defer latch.Done()
+
+	r, err := git.PlainOpen(dir)
+	cuibase.CheckIfError(err)
+	worktree, err := r.Worktree()
+	cuibase.CheckIfError(err)
+	status, err := worktree.Status()
+	cuibase.CheckIfError(err)
+	if status.IsClean() {
+		return
+	}
+
+	temps := strings.Split(dir, "/")
+	fmt.Printf("%v %-20v  %v%v %v\n", cuibase.Yellow, temps[len(temps)-1],
+		cuibase.Green, dir, cuibase.End)
+
+	for filePath := range status {
+		fileStatus := status.File(filePath)
+		var color = cuibase.End
+		if fileStatus.Staging == git.Modified || fileStatus.Worktree == git.Modified {
+			color = cuibase.Cyan
+		}
+		if fileStatus.Staging == git.Deleted || fileStatus.Worktree == git.Deleted {
+			color = cuibase.Red
+		}
+		if fileStatus.Staging == git.Added || fileStatus.Worktree == git.Added {
+			color = cuibase.Green
+		}
+		if fileStatus.Staging == git.Untracked || fileStatus.Worktree == git.Untracked {
+			color = cuibase.Yellow
+		}
+		fmt.Printf("   %v%c%c    %s%s\n", color, fileStatus.Staging, fileStatus.Worktree, filePath, cuibase.End)
+	}
+	fmt.Println()
+}
+
+func getRepoList() []interface{} {
+	home, err := cuibase.Home()
+	cuibase.CheckIfError(err)
+	return cuibase.ReadFileLines(home+"/.repos.sh", func(s string) bool {
+		return strings.Contains(s, "alias") && !strings.Contains(s, "+")
+	}, func(s string) interface{} {
+		temps := strings.Split(s, "'")
+		return RepoAlias{alias: temps[0][5 : len(temps[0])-1], path: temps[1][3:], name: strings.TrimSpace(temps[2][2:])}
+	})
+}
 
 func main() {
 	logger.SetLogPathTrim("/toolbox/")
 	cuibase.RunAction(map[string]func(params []string){
 		"-h": HelpInfo,
-	}, HelpInfo)
+		"l": func(params []string) {
+			list := getRepoList()
+			for i := range list {
+				println(list[i].(RepoAlias).String())
+			}
+		},
+		"pla": func(params []string) {
+			list := getRepoList()
+			var latch sync.WaitGroup
+			for _, s := range list {
+				latch.Add(1)
+				go PullRepo(s.(RepoAlias).path, &latch)
+			}
+			latch.Wait()
+		},
+	}, func(params []string) {
+		list := getRepoList()
+		var latch sync.WaitGroup
+		for _, s := range list {
+			latch.Add(1)
+			go ShowRepoStatus(s.(RepoAlias).path, &latch)
+		}
+		latch.Wait()
+	})
 }
