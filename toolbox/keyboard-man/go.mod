be205108b30f99c66ed5d03f810dacbbb5d5c441
kcp <kuangcp@aliyun.com>
2020-05-01 11:07:19 +0800

    +) store in redis

--- toolbox/keyboard-man/go.mod
+++ toolbox/keyboard-man/go.mod
@@ -3,9 +3,11 @@ module github.com/kuangcp/gobase/keyboard-man
 go 1.14
 
 require (
+	github.com/go-redis/redis v6.15.7+incompatible
 	github.com/gvalkov/golang-evdev v0.0.0-20191114124502-287e62b94bcb
 	github.com/kuangcp/gobase/cuibase v0.0.0-20200409163938-76c18e7b9704
+	github.com/onsi/ginkgo v1.12.0 // indirect
+	github.com/onsi/gomega v1.9.0 // indirect
 )
-
 
 replace github.com/kuangcp/gobase/cuibase => ./../../cuibase
--- toolbox/keyboard-man/main.go
+++ toolbox/keyboard-man/main.go
@@ -3,65 +3,14 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 
+	"github.com/go-redis/redis"
 	. "github.com/gvalkov/golang-evdev"
 	"github.com/kuangcp/gobase/cuibase"
 )
 
-// TODO store in redis
-func Listen(event string) {
-	if event == "" {
-		return
-	}
-
-	device, _ := Open("/dev/input/" + event)
-	defer device.Release()
-
-	//fmt.Println(device)
-	//fmt.Println(device.Capabilities)
-
-	for true {
-		inputEvents, err := device.Read()
-		if err != nil || inputEvents == nil || len(inputEvents) == 0 {
-			continue
-		}
-
-		for _, inputEvent := range inputEvents {
-			event := NewKeyEvent(&inputEvent)
-			if event.State != KeyDown {
-				continue
-			}
-			println(event.String())
-		}
-	}
-}
-
-func HelpInfo(_ []string) {
-	info := cuibase.HelpInfo{
-		Description: "Format markdown file, generate catalog",
-		VerbLen:     -3,
-		ParamLen:    -5,
-		Params: []cuibase.ParamInfo{
-			{
-				Verb:    "-h",
-				Param:   "",
-				Comment: "Help info",
-			}, {
-				Verb:    "-s",
-				Param:   "<device>",
-				Comment: "Listen keyboard with last input device or specific device",
-			}, {
-				Verb:    "-l",
-				Param:   "",
-				Comment: "List keyboard device",
-			}, {
-				Verb:    "-ld",
-				Param:   "",
-				Comment: "List all device",
-			},
-		}}
-	cuibase.Help(info)
-}
+var lastEvent = "last-event"
 
 func main() {
 	cuibase.RunAction(map[string]func(params []string){
@@ -81,11 +30,88 @@ func main() {
 			}
 		},
 		"-s": func(params []string) {
-			if len(params) > 1 {
-				Listen(params[2])
+			if len(params) > 2 {
+				ListenDevice(params[2])
 			} else {
-				Listen("")
+				ListenDevice("")
 			}
 		},
 	}, HelpInfo)
+}
+
+func ListenDevice(event string) {
+	connection := initConnection()
+	if event == "" {
+		last := connection.Get(lastEvent)
+		if last == nil {
+			return
+		}
+		event = last.Val()
+	} else {
+		connection.GetSet(lastEvent, event)
+	}
+	if event == "" {
+		return
+	}
+	device, _ := Open("/dev/input/" + event)
+	defer device.Release()
+
+	//fmt.Println(device)
+	//fmt.Println(device.Capabilities)
+
+	today := time.Now().Format("2006-01-02")
+
+	for true {
+		inputEvents, err := device.Read()
+		if err != nil || inputEvents == nil || len(inputEvents) == 0 {
+			continue
+		}
+
+		for _, inputEvent := range inputEvents {
+			event := NewKeyEvent(&inputEvent)
+			if event.State != KeyDown {
+				continue
+			}
+			//fmt.Printf("%v \n", event)
+			connection.ZIncr(today, redis.Z{Score: 1, Member: event.Scancode})
+			connection.ZIncr("total", redis.Z{Score: 1, Member: today})
+			connection.ZAdd("detail-"+today, redis.Z{Score: float64(inputEvent.Time.Nano()), Member: event.Scancode})
+		}
+	}
+}
+
+func HelpInfo(_ []string) {
+	info := cuibase.HelpInfo{
+		Description: "Format markdown file, generate catalog",
+		VerbLen:     -3,
+		ParamLen:    -5,
+		Params: []cuibase.ParamInfo{
+			{
+				Verb:    "-h",
+				Param:   "",
+				Comment: "Help info",
+			}, {
+				Verb:    "-s",
+				Param:   "<device>",
+				Comment: "Listen keyboard with last device or specific device",
+			}, {
+				Verb:    "-l",
+				Param:   "",
+				Comment: "List keyboard device",
+			}, {
+				Verb:    "-ld",
+				Param:   "",
+				Comment: "List all device",
+			},
+		}}
+	cuibase.Help(info)
+}
+
+func initConnection() *redis.Client {
+	target := redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6667",
+		Password: "",
+		DB:       5,
+	})
+	return target
 }
