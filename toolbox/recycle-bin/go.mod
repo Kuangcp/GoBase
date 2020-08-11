36bf8b86ddc7af90d73843602b952a069d62c3f3
kcp <kuangcp@aliyun.com>
2020-08-11 14:01:35 +0800

    +) recycle bin

--- toolbox/recycle-bin/go.mod
+++ toolbox/recycle-bin/go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/kuangcp/gobase/cuibase v0.0.0-20200530045325-581c22d68355
 	github.com/wonderivan/logger v1.0.0
+	github.com/syndtr/goleveldb v1.0.0
 )
 
 replace github.com/kuangcp/gobase/cuibase => ./../../cuibase
--- toolbox/recycle-bin/main.go
+++ toolbox/recycle-bin/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
-	"github.com/kuangcp/gobase/cuibase"
-	"github.com/wonderivan/logger"
 	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/kuangcp/gobase/cuibase"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/wonderivan/logger"
 )
 
-var trashDir = ".config/app-conf/recycle-bin"
+var mainDir = ".config/app-conf/recycle-bin"
+var trashDir = mainDir + "/trash"
+var configDir = mainDir + "/conf"
+var dbDir = configDir + "/db"
 
 var info = cuibase.HelpInfo{
 	Description: "Recycle bin",
@@ -23,22 +32,42 @@ var info = cuibase.HelpInfo{
 			Verb:    "",
 			Param:   "file",
 			Comment: "Delete file",
-			Handler: DeleteFile,
 		}, {
 			Verb:    "-as",
 			Param:   "suffix",
 			Comment: "delete *.suffix",
-			Handler: DeleteFileBySuffix,
 		},
 	}}
 
+var (
+	help   bool
+	suffix string
+)
+
+func init() {
+	flag.BoolVar(&help, "h", false, "")
+	flag.StringVar(&suffix, "as", "", "")
+}
+
 func main() {
+	flag.Parse()
+
+	if help {
+		info.PrintHelp()
+		return
+	}
+
 	logger.SetLogPathTrim("recycle-bin")
 	home, err := cuibase.Home()
 	cuibase.CheckIfError(err)
 	trashDir = home + "/" + trashDir
 
-	cuibase.RunActionFromInfo(info, DeleteFile)
+	if suffix != "" {
+		DeleteFile(strings.Split(suffix, ","))
+		return
+	}
+
+	DeleteFile(os.Args)
 }
 
 func PathExists(path string) (bool, error) {
@@ -64,18 +93,41 @@ func DeleteFile(params []string) {
 	} else {
 		filepath = params[2]
 	}
-	logger.Info(filepath, "move to", trashDir)
 
-	exists, err := PathExists(filepath)
-	cuibase.CheckIfError(err)
-	if !exists {
-		logger.Error(filepath, "not found")
+	deleteFiles(filepath)
+}
+
+// deleteFies 移动文件到回收站，并通过LevelDB存储时间等信息
+func deleteFiles(files ...string) {
+	db, err := leveldb.OpenFile(dbDir, nil)
+	defer db.Close()
+	if err != nil {
+		logger.Error(err)
 		return
 	}
 
-	err = os.Rename(filepath, trashDir+"/"+filepath)
-	if err != nil {
-		logger.Error(err)
+	// TODO 多个同名文件处理方式
+	for _, filepath := range files {
+		logger.Info(filepath, "move to", trashDir)
+
+		exists, err := PathExists(filepath)
+		cuibase.CheckIfError(err)
+		if !exists {
+			logger.Error(filepath, "not found")
+			return
+		}
+
+		cmd := exec.Command("mv", filepath, trashDir+"/"+filepath)
+		var out bytes.Buffer
+
+		cmd.Stdout = &out
+		err = cmd.Run()
+		if err != nil {
+			logger.Error(err)
+			return
+		}
+
+		db.Put([]byte(filepath), []byte(""), nil)
 	}
 }
 
