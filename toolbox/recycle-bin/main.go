4336973fbfd4bda5050d8533b337a232357fbbac
kcp <kuangcp@aliyun.com>
2020-11-09 16:21:45 +0800

    *) extract

--- toolbox/recycle-bin/main.go
+++ toolbox/recycle-bin/main.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -17,6 +16,10 @@ import (
 
 	"github.com/kuangcp/gobase/pkg/cuibase"
 	"github.com/wonderivan/logger"
+)
+
+const (
+	emptyMaxLoop = 10
 )
 
 var (
@@ -40,53 +43,6 @@ var (
 	liveStr  string // time.ParseDuration()
 	checkStr string
 )
-var info = cuibase.HelpInfo{
-	Description:   "Recycle bin",
-	Version:       "1.0.2",
-	SingleFlagLen: -3,
-	ValueLen:      -10,
-	Flags: []cuibase.ParamVO{
-		{
-			Short:   "-h",
-			Value:   "",
-			Comment: "Help info and init",
-		}, {
-			Short:   "-D",
-			Value:   "",
-			Comment: "Debug mode",
-		}, {
-			Short:   "-X",
-			Value:   "",
-			Comment: "Exit daemon",
-		}, {
-			Short:   "-C",
-			Value:   "",
-			Comment: "Start check",
-		}, {
-			Short:   "-d",
-			Value:   "",
-			Comment: "Start check by daemon",
-		},
-	},
-	Options: []cuibase.ParamVO{
-		{
-			Short:   "",
-			Value:   "file",
-			Comment: "Delete file",
-		}, {
-			Short:   "-s",
-			Value:   "suffix",
-			Comment: "Delete *.suffix",
-		}, {
-			Short:   "-l",
-			Value:   "duration",
-			Comment: "File live time, default 1m. (unit: ms/s/m/h) ",
-		}, {
-			Short:   "-c",
-			Value:   "duration",
-			Comment: "Check trash period, default 1m. (unit: ms/s/m/h)",
-		},
-	}}
 
 func init() {
 	logger.SetLogPathTrim("recycle-bin")
@@ -110,8 +66,8 @@ func init() {
 	flag.BoolVar(&daemon, "d", false, "")
 	flag.BoolVar(&exit, "X", false, "")
 
-	flag.StringVar(&liveStr, "l", "1m", "")
-	flag.StringVar(&checkStr, "c", "1m", "")
+	flag.StringVar(&liveStr, "l", "30s", "")
+	flag.StringVar(&checkStr, "c", "5s", "")
 	flag.StringVar(&suffix, "s", "", "")
 
 	flag.Usage = info.PrintHelp
@@ -174,27 +130,96 @@ func checkTrashDir() {
 		return
 	}
 
-	checkTrashDaemon()
+	exists, err := isPathExists(pidFile)
+	if exists {
+		return
+	}
+	logger.Debug("Start check trash, period:", checkPeriod, "pid:", os.Getpid())
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+
+	// avoid repeat delete
+	var deleteFlag int32 = 0
+
+	go func() {
+		// Wait for interrupt signal to gracefully shutdown the server with
+		// a timeout of 5 seconds.
+		quit := make(chan os.Signal)
+		// kill (no param) default send syscall.SIGTERM
+		// kill -2 is syscall.SIGINT
+		// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+
+		logger.Warn("killed")
+		deletePidFile(&deleteFlag)
+		os.Exit(1)
+	}()
+
+	// create pid
+	err = ioutil.WriteFile(pidFile, []byte(strconv.Itoa(os.Getpid())), 0644)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+
+	defer deletePidFile(&deleteFlag)
+
+	emptyCount := 0
+	for true {
+		current := time.Now().UnixNano()
+		time.Sleep(checkPeriod)
+		logger.Info("check")
+		dir, err := ioutil.ReadDir(trashDir)
+		if err != nil {
+			return
+		}
+
+		if len(dir) == 0 {
+			emptyCount++
+		}
+		if emptyCount > emptyMaxLoop {
+			return
+		}
+
+		for _, fileInfo := range dir {
+			name := fileInfo.Name()
+			index := strings.Index(name, ".T.")
+			if index == -1 {
+				continue
+			}
+
+			value := name[index+3:]
+			//fmt.Println(value)
+			parseInt, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				logger.Error(err)
+				return
+			}
+
+			//logger.Debug(current, parseInt, current-parseInt)
+			if current-parseInt > liveTime.Nanoseconds() {
+				logger.Info("delete: ", name[:index])
+				actualPath := trashDir + "/" + name
+				if actualPath == "/" {
+					logger.Error("danger error")
+					continue
+				}
+				cmd := exec.Command("rm", "-rf", actualPath)
+				execCmdWithQuite(cmd)
+			}
+		}
+	}
 }
 
-func initDir() {
-	err := os.MkdirAll(trashDir, 0755)
-	cuibase.CheckIfError(err)
-	err = os.MkdirAll(configDir, 0755)
-	cuibase.CheckIfError(err)
-	err = os.MkdirAll(logDir, 0755)
-	cuibase.CheckIfError(err)
-}
-
-func isPathExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
+func deletePidFile(deleteFlag *int32) {
+	logger.Warn("exit")
+	curDelete := atomic.AddInt32(deleteFlag, 1)
+	if curDelete == 1 {
+		actualDeleteFile(pidFile)
 	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
 }
 
 // deleteFies 移动文件到回收站
@@ -239,124 +264,6 @@ func exitCheckFileDaemon() {
 	logger.Info("kill ", pid)
 	cmd := exec.Command("kill", pid)
 	execCmdWithQuite(cmd)
-}
-
-func NewSysProcAttr() *syscall.SysProcAttr {
-	return &syscall.SysProcAttr{
-		Setsid: true,
-	}
-}
-
-func startProc(args []string, logFile string) (*exec.Cmd, error) {
-	cmd := &exec.Cmd{
-		Path:        args[0],
-		Args:        args,
-		SysProcAttr: NewSysProcAttr(),
-	}
-
-	if logFile != "" {
-		stdout, err := os.OpenFile(logFile, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
-		if err != nil {
-			log.Println(os.Getpid(), ": 打开日志文件错误:", err)
-			return nil, err
-		}
-		cmd.Stderr = stdout
-		cmd.Stdout = stdout
-	}
-
-	err := cmd.Start()
-	if err != nil {
-		return nil, err
-	}
-
-	return cmd, nil
-}
-
-func checkTrashDaemon() {
-	exists, err := isPathExists(pidFile)
-	if exists {
-		return
-	}
-	logger.Debug("Start check trash")
-	if err != nil {
-		logger.Error(err)
-		return
-	}
-
-	// avoid repeat delete
-	var deleteFlag int32 = 0
-
-	logger.Debug("process", os.Getpid())
-	go func() {
-		// Wait for interrupt signal to gracefully shutdown the server with
-		// a timeout of 5 seconds.
-		quit := make(chan os.Signal)
-		// kill (no param) default send syscall.SIGTERM
-		// kill -2 is syscall.SIGINT
-		// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
-		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-		<-quit
-
-		logger.Warn("killed")
-		curDelete := atomic.AddInt32(&deleteFlag, 1)
-		if curDelete == 1 {
-			actualDeleteFile(pidFile)
-		}
-		os.Exit(1)
-	}()
-
-	// create pid
-	err = ioutil.WriteFile(pidFile, []byte(strconv.Itoa(os.Getpid())), 0644)
-	if err != nil {
-		logger.Error(err)
-		return
-	}
-
-	// delete pid
-	defer func() {
-		curDelete := atomic.AddInt32(&deleteFlag, 1)
-		if curDelete == 1 {
-			actualDeleteFile(pidFile)
-		}
-	}()
-
-	for true {
-		current := time.Now().UnixNano()
-		time.Sleep(checkPeriod)
-		logger.Info("check ...", checkPeriod, os.Getpid())
-		dir, err := ioutil.ReadDir(trashDir)
-		if err != nil {
-			return
-		}
-
-		for _, fileInfo := range dir {
-			name := fileInfo.Name()
-			index := strings.Index(name, ".T.")
-			if index == -1 {
-				continue
-			}
-
-			value := name[index+3:]
-			//fmt.Println(value)
-			parseInt, err := strconv.ParseInt(value, 10, 64)
-			if err != nil {
-				logger.Error(err)
-				return
-			}
-
-			//logger.Debug(current, parseInt, current-parseInt)
-			if current-parseInt > liveTime.Nanoseconds() {
-				logger.Info("delete: ", name[:index])
-				actualPath := trashDir + "/" + name
-				if actualPath == "/" {
-					logger.Error("danger error")
-					continue
-				}
-				cmd := exec.Command("rm", "-rf", actualPath)
-				execCmdWithQuite(cmd)
-			}
-		}
-	}
 }
 
 func actualDeleteFile(path string) {
---
+++ toolbox/recycle-bin/util.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+
+	"github.com/kuangcp/gobase/pkg/cuibase"
+	"github.com/wonderivan/logger"
+)
+
+var info = cuibase.HelpInfo{
+	Description:   "Recycle bin",
+	Version:       "1.0.2",
+	SingleFlagLen: -3,
+	ValueLen:      -10,
+	Flags: []cuibase.ParamVO{
+		{
+			Short:   "-h",
+			Value:   "",
+			Comment: "Help info and init",
+		}, {
+			Short:   "-D",
+			Value:   "",
+			Comment: "Debug mode",
+		}, {
+			Short:   "-X",
+			Value:   "",
+			Comment: "Exit daemon",
+		}, {
+			Short:   "-C",
+			Value:   "",
+			Comment: "Start check",
+		}, {
+			Short:   "-d",
+			Value:   "",
+			Comment: "Start check by daemon",
+		},
+	},
+	Options: []cuibase.ParamVO{
+		{
+			Short:   "",
+			Value:   "file",
+			Comment: "Delete file",
+		}, {
+			Short:   "-s",
+			Value:   "suffix",
+			Comment: "Delete *.suffix",
+		}, {
+			Short:   "-l",
+			Value:   "duration",
+			Comment: "File live time, default 1m. (unit: ms/s/m/h) ",
+		}, {
+			Short:   "-c",
+			Value:   "duration",
+			Comment: "Check trash period, default 1m. (unit: ms/s/m/h)",
+		},
+	}}
+
+func initDir() {
+	err := os.MkdirAll(trashDir, 0755)
+	cuibase.CheckIfError(err)
+	err = os.MkdirAll(configDir, 0755)
+	cuibase.CheckIfError(err)
+	err = os.MkdirAll(logDir, 0755)
+	cuibase.CheckIfError(err)
+}
+
+func isPathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func newSysProcAttr() *syscall.SysProcAttr {
+	return &syscall.SysProcAttr{
+		Setsid: true,
+	}
+}
+
+func startProc(args []string, logFile string) (*exec.Cmd, error) {
+	cmd := &exec.Cmd{
+		Path:        args[0],
+		Args:        args,
+		SysProcAttr: newSysProcAttr(),
+	}
+
+	if logFile != "" {
+		stdout, err := os.OpenFile(logFile, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+		if err != nil {
+			logger.Error("打开日志文件错误: ", os.Getpid(), err)
+			return nil, err
+		}
+		cmd.Stderr = stdout
+		//cmd.Stdout = stdout
+	}
+
+	err := cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	return cmd, nil
+}
