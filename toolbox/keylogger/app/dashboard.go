27c3081043328b094a17aa6d8894097deb87a33f
Kuangcp <kuangcp@aliyun.com>
2021-01-18 23:49:49 +0800

    *) use label replace draw

--- toolbox/keylogger/app/dashboard.go
+++ toolbox/keylogger/app/dashboard.go
@@ -2,12 +2,12 @@ package app
 
 import (
 	"fmt"
+	"github.com/kuangcp/gobase/pkg/cuibase"
 	"log"
 	"time"
 	"unsafe"
 
 	"github.com/go-redis/redis"
-	"github.com/gotk3/gotk3/cairo"
 	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
@@ -17,37 +17,34 @@ import (
 const (
 	width           = 128
 	height          = 24
-	refreshPeriod   = time.Millisecond * 300
+	refreshPeriod   = time.Millisecond * 520
 	recordThreshold = 58 // xx s后才存储 bpm
 )
 
 var (
+	bpmLabel   *gtk.Label
 	firstStart = true
 )
 
 func ShowWindow() {
 	gtk.Init(nil)
-
 	app, _ := gtk.ApplicationNew("com.github.kuangcp.keylogger", glib.APPLICATION_FLAGS_NONE)
 	_, err := app.Connect("activate", func() {
 		createWindow(app)
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	cuibase.CheckIfError(err)
 	app.Run(nil)
 }
 
 func createWindow(app *gtk.Application) {
 	win, _ := gtk.WindowNew(gtk.WINDOW_POPUP)
+	win.Add(windowWidget())
 	app.AddWindow(win)
-	area, _ := gtk.DrawingAreaNew()
 
-	win.Add(area)
 	win.SetTitle("Dashboard")
 	win.SetDefaultSize(width, height)
-	win.Connect("destroy", gtk.MainQuit)
+	_, err := win.Connect("destroy", gtk.MainQuit)
+	cuibase.CheckIfError(err)
 	win.SetPosition(gtk.WIN_POS_MOUSE)
 	win.ShowAll()
 
@@ -74,39 +71,43 @@ func createWindow(app *gtk.Application) {
 	_, _ = win.Connect("motion-notify-event", func(widget *gtk.Window, ctx *gdk.Event) {
 		//获取鼠标移动属性结构体变量，系统内部的变量，不是用户传参变量
 		event := *(*gdk.EventButton)(unsafe.Pointer(&ctx))
-		//fmt.Println(x, int(event.XRoot())-x, y,int(event.YRoot())-y)
 		win.Move(int(event.XRoot())-x, int(event.YRoot())-y)
 	})
 
-	area.Connect("draw", drawHandler)
-
 	go func() {
-		for true {
+		for {
 			time.Sleep(refreshPeriod)
-			win.QueueDraw()
+			total, bpm, todayMax := buildShowData()
+
+			str := fmt.Sprintf("<span foreground='green' font_desc='16'>%d</span> <span font_desc='15'>%d</span> <span foreground='yellow' font_desc='10'>%d</span>",
+				bpm, total, todayMax)
+			_, err := glib.IdleAdd(bpmLabel.SetMarkup, str)
+			if err != nil {
+				log.Fatal("IdleAdd() failed:", err)
+			}
 		}
 	}()
 }
 
-func drawHandler(da *gtk.DrawingArea, cr *cairo.Context) {
-	total, bpm, todayMax := buildShowData()
+func windowWidget() *gtk.Widget {
+	grid, err := gtk.GridNew()
+	if err != nil {
+		log.Fatal("Unable to create grid:", err)
+	}
+	grid.SetOrientation(gtk.ORIENTATION_VERTICAL)
 
-	cr.MoveTo(3, 18)
+	bpmLabel, err = gtk.LabelNew("")
+	if err != nil {
+		log.Fatal("Unable to create label:", err)
+	}
 
-	cr.SetSourceRGB(0, 150, 0)
-	cr.SetFontSize(18)
-	cr.ShowText(fmt.Sprintf("%3d ", bpm))
+	grid.Add(bpmLabel)
+	bpmLabel.SetHExpand(false)
+	bpmLabel.SetVExpand(true)
 
-	cr.SetSourceRGB(255, 255, 255)
-	cr.SetFontSize(18)
-	cr.ShowText(fmt.Sprintf("%-5d ", total))
-
-	cr.SetSourceRGB(150, 150, 0)
-	cr.SetFontSize(13)
-	cr.ShowText(fmt.Sprintf("%-3d ", todayMax))
-
-	cr.Fill()
+	return &grid.Container.Widget
 }
+
 func buildShowData() (int, int, int) {
 	conn := GetConnection()
 	now := time.Now()
