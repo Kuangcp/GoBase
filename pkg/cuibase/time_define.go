c0674fc943f08219bf74295a3d76082b8401c65b
Kuangcp <kuangcp@aliyun.com>
2021-04-04 09:02:49 +0800

    +) time format

---
+++ pkg/cuibase/time_define.go
@@ -0,0 +1,11 @@
+package cuibase
+
+const (
+	YYYY                   = "2006"
+	YYYY_MM                = "2006-01"
+	YYYY_MM_DD             = "2006-01-02"
+	YYYY_MM_DD_HH          = "2006-01-02 15"
+	YYYY_MM_DD_HH_MM       = "2006-01-02 15:04"
+	YYYY_MM_DD_HH_MM_SS    = "2006-01-02 15:04:05"
+	YYYY_MM_DD_HH_MM_SS_MI = "2006-01-02 15:04:05.000"
+)
