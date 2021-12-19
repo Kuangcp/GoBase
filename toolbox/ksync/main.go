f8bb736400df97fa8968452459e97f026d20d6c8
Kuangcp <kuangcp@aliyun.com>
2021-12-20 01:49:16 +0800

    +) support sync file

--- toolbox/ksync/main.go
+++ toolbox/ksync/main.go
@@ -5,38 +5,33 @@ import (
 	"flag"
 	"fmt"
 	"github.com/getlantern/systray"
+	"github.com/kuangcp/logger"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"time"
 )
 
 //go:embed sync.png
 var iconImg string
 
-var serverList []string
-var port int
-var version bool
+var (
+	sideList []string // 对端列表 格式 host:port
+	port     int
+	version  bool
+	initSide string
+)
+
+var (
+	lastMod = time.Now()
+)
 
 func init() {
-	flag.IntVar(&port, "p", 8000, "")
-	flag.BoolVar(&version, "v", false, "")
-}
-
-func checkModFile() {
-	dir, err := ioutil.ReadDir("./")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	for _, info := range dir {
-		if info.IsDir() {
-			fmt.Println("dir", info)
-			continue
-		}
-		fmt.Println(info.Name(), info.ModTime())
-	}
-	fmt.Println()
+	flag.IntVar(&port, "p", 8000, "port")
+	flag.BoolVar(&version, "v", false, "version")
+	flag.StringVar(&initSide, "s", "", "init side url")
 }
 
 func main() {
@@ -46,25 +41,101 @@ func main() {
 		return
 	}
 
-	//go func() {
-	//	ticker := time.NewTicker(time.Second * 3)
-	//	for range ticker.C {
-	//		checkModFile()
-	//	}
-	//}()
+	initSideBind()
 
-	go func() {
-		http.HandleFunc("/register", func(writer http.ResponseWriter, request *http.Request) {
-			client := request.Header.Get("self")
-			serverList = append(serverList, client)
-			writer.Write([]byte("OK"))
-		})
-
-		err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
-		if err != nil {
-			log.Fatal("error: ", err)
-		}
-	}()
+	go webServer()
 
 	systray.Run(OnReady, OnExit)
+}
+
+func syncFile() []string {
+	var result []string
+	dir, err := ioutil.ReadDir("./")
+	if err != nil {
+		fmt.Println(err)
+		return result
+	}
+
+	for _, info := range dir {
+		if info.IsDir() {
+			//fmt.Println("dir", info)
+			continue
+		}
+		if lastMod.Before(info.ModTime()) {
+			logger.Info("need sync", info.Name(), info.ModTime())
+			result = append(result, info.Name())
+		}
+	}
+	if len(result) != 0 {
+		lastMod = time.Now()
+	}
+	return result
+}
+
+func webServer() {
+	http.HandleFunc("/sync", func(writer http.ResponseWriter, request *http.Request) {
+		query, err := url.ParseQuery(request.URL.RawQuery)
+		if err != nil {
+			logger.Error(err)
+			return
+		}
+
+		name := query.Get("name")
+		unescape, err := url.QueryUnescape(name)
+		if err != nil {
+			logger.Error(err)
+			return
+		}
+
+		open, err := os.Create(unescape)
+		if err != nil {
+			logger.Error(err)
+			return
+		}
+
+		var buf = make([]byte, 4096)
+		for {
+			read, err := request.Body.Read(buf)
+			if read != 0 {
+				open.Write(buf[:read])
+			}
+			if err != nil {
+				break
+			}
+		}
+
+		open.Close()
+	})
+	http.HandleFunc("/register", func(writer http.ResponseWriter, request *http.Request) {
+		client := request.Header.Get("self")
+		logger.Info("add new", client)
+		sideList = append(sideList, client)
+		writer.Write([]byte("OK"))
+	})
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal("error: ", err)
+	}
+}
+
+func initSideBind() {
+	if initSide == "" {
+		return
+	}
+
+	client := http.Client{}
+	req, err := http.NewRequest("GET", "http://"+initSide+"/register", nil)
+	req.Header.Set("self", fmt.Sprintf("localhost:%v", port))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	rsp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(rsp)
+	sideList = append(sideList, initSide)
 }
--- toolbox/ksync/tray.go
+++ toolbox/ksync/tray.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/getlantern/systray"
 	"github.com/kuangcp/logger"
 	"ksync/icon"
+	"net/http"
+	"net/url"
+	"os"
 	"runtime"
 )
 
@@ -16,7 +20,23 @@ func OnReady() {
 	systray.SetTitle("K-Sync")
 	systray.SetTooltip("K-Sync")
 
-	exitItem := systray.AddMenuItem("Exit", "Exit the whole app")
+	syncItem := systray.AddMenuItem("Sync", "sync file")
+	go func() {
+		for {
+			select {
+			case <-syncItem.ClickedCh:
+				logger.Info("sync %v", sideList)
+				fileList := syncFile()
+				for _, path := range fileList {
+					for _, r := range sideList {
+						postFile(r, path)
+					}
+				}
+			}
+		}
+	}()
+
+	exitItem := systray.AddMenuItem(fmt.Sprintf("Exit (%v)", port), "Exit the whole app")
 	go func() {
 		<-exitItem.ClickedCh
 		logger.Info("Requesting quit")
@@ -24,16 +44,28 @@ func OnReady() {
 		logger.Info("Finished quitting")
 	}()
 
-	systray.AddSeparator()
-	syncItem := systray.AddMenuItem("Sync", "sync file")
-	go func() {
-		for {
-			select {
-			case <-syncItem.ClickedCh:
-				logger.Info("sync")
-			}
-		}
-	}()
+}
+
+func postFile(server string, path string) {
+	logger.Info(path, "post to", server)
+	client := http.Client{}
+	open, err := os.Open(path)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+
+	defer open.Close()
+	name := url.QueryEscape(path)
+	syncURL := "http://" + server + "/sync?name=" + name
+	//fmt.Println(syncURL)
+	post, err := client.Post(syncURL, "", open)
+	if err != nil {
+		return
+	}
+
+	defer post.Body.Close()
+	logger.Info(post, err)
 }
 
 func OnExit() {
