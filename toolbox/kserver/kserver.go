7ed0b277611cc79d3c619f81981ee412686d82a0
Kuangcp <kuangcp@aliyun.com>
2022-01-09 00:02:13 +0800

    *) split handler

--- toolbox/kserver/kserver.go
+++ toolbox/kserver/kserver.go
@@ -1,21 +1,14 @@
 package main
 
 import (
+	_ "embed"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
-	"mime/multipart"
 	"net"
 	"net/http"
-	"net/url"
-	"os"
 	"sort"
 	"strings"
-	"time"
-
-	_ "embed"
 
 	"github.com/kuangcp/gobase/cuibase"
 )
@@ -76,84 +69,6 @@ func getInternalIP() string {
 	return ""
 }
 
-func isFileExist(filename string) bool {
-	_, err := os.Stat(filename)
-	return err == nil
-}
-
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	var maxMib int64 = 10
-	err := r.ParseMultipartForm(maxMib << 20)
-	if err != nil {
-		log.Println(err)
-	}
-
-	for _, headers := range r.MultipartForm.File {
-		for _, header := range headers {
-			if err := receiveFile(header); err != nil {
-				return
-			}
-		}
-	}
-
-	http.Redirect(w, r, "/up", http.StatusMovedPermanently)
-}
-
-func receiveFile(header *multipart.FileHeader) error {
-	filename := header.Filename
-	log.Printf("upload: %s", filename)
-
-	exist := isFileExist(filename)
-	if exist {
-		filename = fmt.Sprint(time.Now().Nanosecond()) + "-" + filename
-	}
-
-	dst, err := os.Create(filename)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	open, err := header.Open()
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	defer func() {
-		err := dst.Close()
-		if err != nil {
-			log.Println(err)
-		}
-	}()
-
-	defer func() {
-		err := open.Close()
-		if err != nil {
-			log.Println(err)
-		}
-	}()
-
-	_, err = io.Copy(dst, open)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-	return nil
-}
-
-func echoHandler(_ http.ResponseWriter, request *http.Request) {
-	body, _ := ioutil.ReadAll(request.Body)
-	content := string(body)
-
-	decode, _ := url.QueryUnescape(content)
-	if strings.HasPrefix(decode, "content") {
-		decode = decode[8:]
-	}
-	decode = "Content: \n" + decode
-	log.Print(decode)
-}
-
 func printStartUpLog(port int, internalIP string) {
 	innerURL := fmt.Sprintf("http://%v:%v", internalIP, port)
 	log.Printf("static file web server has started.\n")
@@ -192,57 +107,6 @@ func bindPathAndStatic(pattern, binContent string) {
 			log.Println(err)
 		}
 	})
-}
-
-func buildImgFunc(parentPath string) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		dir, err := os.ReadDir(pathDirMap[parentPath])
-		if err != nil {
-			w.Write([]byte("error"))
-			return
-		}
-		w.Write([]byte(`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Img</title>
-<style>
-	img {
-	width: 210px;
-	max-height: 100px;
-	}
-</style>
-</head>
-<body>`))
-
-		sort.Slice(dir, func(i, j int) bool {
-			iInfo, _ := dir[i].Info()
-			jInfo, _ := dir[j].Info()
-			return iInfo.ModTime().After(jInfo.ModTime())
-		})
-
-		for _, entry := range dir {
-			fileInfo, _ := entry.Info()
-			fileInfo.ModTime()
-			if entry.IsDir() {
-				continue
-			}
-
-			fileName := entry.Name()
-			idx := strings.LastIndex(fileName, ".")
-			if idx == -1 {
-				w.Write([]byte("<img  src=\"" + fileName + "\" alt=\"" + fileName + "\">"))
-				continue
-			}
-			suffixType := fileName[idx:]
-			fmt.Println(suffixType)
-			if suffixType == ".jpg" || suffixType == ".png" || suffixType == ".svg" || suffixType == ".webp" ||
-				suffixType == ".bmp" || suffixType == ".gif" || suffixType == ".ico" {
-				w.Write([]byte("<img  src=\"" + fileName + "\" alt=\"" + fileName + "\">"))
-			}
-		}
-		w.Write([]byte(`</body></html>`))
-	}
 }
 
 var info = cuibase.HelpInfo{
