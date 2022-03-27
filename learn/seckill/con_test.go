714da55d86915f40b8f62f356a9c8c40391d4c2e
Kuangcp <kuangcp@aliyun.com>
2022-03-28 01:07:25 +0800

    fix: con

--- learn/seckill/con_test.go
+++ learn/seckill/con_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-redis/redis"
 	"github.com/kuangcp/logger"
 	"io/ioutil"
 	"math/rand"
@@ -11,6 +12,9 @@ import (
 )
 
 var skuId string
+var secKillTime = 8
+var delaySec = 5
+var startSec, endSec int
 
 func init() {
 	_ = logger.SetLoggerConfig(&logger.LogConfig{
@@ -20,34 +24,75 @@ func init() {
 		},
 		TimeFormat: logger.LogTimeDetailFormat,
 	})
-	skuId = fmt.Sprint(time.Now().UnixMilli())
+	skuId = fmt.Sprint(9999999999999 - time.Now().UnixMilli())
 
 	http.Get("http://localhost:8099/reg?skuId=" + skuId)
+	second := time.Now().Second()
+	startSec = second + delaySec
+	endSec = second + delaySec + secKillTime
 }
 
-func cancel(id int) {
-	if rand.Intn(100)%3 != 0 {
+func TestBuy(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 500; j++ {
+			go secBuy(i*1000 + j)
+			go cancel(i*1000+j, 7)
+		}
+		//time.Sleep(time.Millisecond * 500)
+	}
+
+	fmt.Println(startSec, endSec)
+	time.Sleep(time.Second * time.Duration(delaySec+secKillTime+30))
+}
+
+func cancel(id, cancelRate int) {
+	if rand.Intn(1000)%cancelRate != 0 {
 		return
 	}
 
-	time.Sleep(time.Millisecond * time.Duration((200+id)%60))
-	userId := fmt.Sprint("user", id)
-	rsp, err := http.Get("http://localhost:8099/cancel?skuId=" + skuId + "&userId=" + userId)
-	if err != nil {
-		return
+	//fmt.Println(id)
+	for {
+		second := time.Now().Second()
+		if second > endSec {
+			logger.Error("timeout")
+			break
+		}
+
+		if second < startSec {
+			time.Sleep(time.Millisecond * 300)
+			continue
+		}
+
+		time.Sleep(time.Millisecond * time.Duration((200+id)%360+200))
+		userId := fmt.Sprint("user", id)
+		rsp, err := http.Get("http://localhost:8099/cancel?skuId=" + skuId + "&userId=" + userId)
+		if err != nil {
+			return
+		}
+		rsp.Body.Close()
+		break
 	}
-	rsp.Body.Close()
 }
 
 func secBuy(id int) {
 	userId := fmt.Sprint("user", id)
 	for {
+		second := time.Now().Second()
+		if second > endSec {
+			logger.Error("timeout")
+			break
+		}
+		if second < startSec {
+			time.Sleep(time.Millisecond * 500)
+			//logger.Info("waiting")
+			continue
+		}
 		rsp, err := http.Get("http://localhost:8099/buy?skuId=" + skuId + "&userId=" + userId)
 		if err != nil {
 			continue
 		}
 
-		time.Sleep(time.Millisecond * time.Duration((id+333)%295))
+		//time.Sleep(time.Millisecond * time.Duration((id+333)%200))
 		all, err := ioutil.ReadAll(rsp.Body)
 		if err != nil {
 			continue
@@ -56,7 +101,7 @@ func secBuy(id int) {
 		rspStr := string(all)
 		if rspStr == "OK" {
 			//logger.Info("ok", userId)
-			fmt.Print(id, ",")
+			//fmt.Print(id, ",")
 			break
 		}
 		if rspStr == "SALE_OUT" {
@@ -66,27 +111,16 @@ func secBuy(id int) {
 	}
 }
 
-func TestBuy(t *testing.T) {
-	logger.Warn("start new")
-	for i := 0; i < 1000; i++ {
-		go secBuy(i)
-		go cancel(i)
-	}
-
-	time.Sleep(time.Second * 4)
-	logger.Warn("start new")
-	for i := 1000; i < 2500; i++ {
-		go secBuy(i)
-	}
-	time.Sleep(time.Second * 4)
-}
-
 func TestRand(t *testing.T) {
 	for i := 0; i < 299; i++ {
 		fmt.Println(rand.Intn(100))
 	}
 }
 
-func TestRename(t *testing.T) {
+func TestSet(t *testing.T) {
+	option := redis.Options{Addr: "127.0.0.1:8834", DB: 0}
+	connection = redis.NewClient(&option)
 
+	connection.Set("ab", 3, 0)
+	connection.Incr("ab")
 }
--- learn/seckill/main.go
+++ learn/seckill/main.go
@@ -4,14 +4,13 @@ import (
 	"github.com/go-redis/redis"
 	"github.com/kuangcp/logger"
 	"net/http"
-	"strconv"
 	"time"
 )
 
 const (
 	goodsKey    = "sec_goods_key:"
 	goodsRegKey = "sec_goods_key_reg"
-	amount      = 500
+	amount      = 3000
 )
 
 type (
@@ -43,32 +42,42 @@ func init() {
 }
 
 func sync() {
-	for range time.NewTicker(time.Millisecond * 200).C {
+	for range time.NewTicker(time.Millisecond * 1000).C {
 		latestSku, err := connection.LIndex(goodsRegKey, 0).Result()
 		if err != nil {
 			continue
 		}
+
 		skuCountKey := getCountKey(latestSku)
 		i, err := connection.Get(skuCountKey).Result()
 		if i == "" {
 			logger.Warn("not exist")
 			continue
 		}
-		curAmount, _ := strconv.Atoi(i)
-		//if err != nil || curAmount < amount*1.2 {
-		//	//logger.Warn("not full")
-		//	continue
-		//}
-		result, err := connection.HLen(getHisKey(latestSku)).Result()
+
+		successCount, err := connection.HLen(getHisKey(latestSku)).Result()
 		if err != nil {
 			logger.Error(latestSku, err)
 			continue
 		}
-		if result < amount {
-			logger.Warn(skuCountKey, "reset to", result)
-			connection.IncrBy(skuCountKey, -1*(int64(curAmount)-result))
-			//connection.Set(skuCountKey, result, 0)
+
+		if successCount < amount {
+			logger.Warn(skuCountKey, "reset to", successCount)
+			connection.Set(skuCountKey, successCount, 0)
 		}
+
+		// validate
+		cancelCount, err := connection.SCard(getCancelKey(latestSku)).Result()
+		if err != nil {
+			continue
+		}
+
+		failedCount, err := connection.SCard(getFailedKey(latestSku)).Result()
+		if err != nil {
+			continue
+		}
+
+		logger.Debug(failedCount + cancelCount + successCount)
 	}
 }
 
@@ -80,6 +89,7 @@ func main() {
 			return
 		}
 		connection.LPush(goodsRegKey, skuId)
+		logger.Info("reg", skuId)
 	})
 
 	http.HandleFunc("/cancel", func(writer http.ResponseWriter, request *http.Request) {
@@ -99,13 +109,13 @@ func main() {
 		}
 
 		skuKey := getCountKey(param.SkuId)
-		i, err := connection.IncrBy(skuKey, -1).Result()
+		_, err = connection.IncrBy(skuKey, -1).Result()
 		if err != nil {
 			return
 		}
 		connection.HDel(hisKey, param.UserId)
 		connection.SAdd(getCancelKey(param.SkuId), param.UserId)
-		logger.Info("cancel:", i)
+		//logger.Info("cancel:", i)
 	})
 
 	http.HandleFunc("/buy", func(writer http.ResponseWriter, request *http.Request) {
@@ -115,6 +125,7 @@ func main() {
 			return
 		}
 
+		// 进入先减库存
 		result, err := connection.Incr(getCountKey(param.SkuId)).Result()
 		if err != nil {
 			logger.Error(err)
@@ -134,7 +145,7 @@ func main() {
 			writer.Write([]byte("error"))
 			return
 		}
-		// 恢复库存
+		// 已购买情况，恢复库存
 		if userAlreadyBuy {
 			_, err := connection.IncrBy(getCountKey(param.SkuId), -1).Result()
 			if err != nil {
