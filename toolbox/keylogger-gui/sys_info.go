071ef3b83ad6fa2aaf316ad1209b11f11ec3c37f
kuangcp <kuangcp@aliyun.com>
2023-04-10 20:11:08 +0800

    gui: simplifi

--- toolbox/keylogger-gui/sys_info.go
+++ toolbox/keylogger-gui/sys_info.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gotk3/gotk3/cairo"
+	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 	"github.com/shirou/gopsutil/mem"
 	"log"
@@ -11,6 +11,7 @@ import (
 
 const (
 	refreshSec = 1
+	lineWeight = 1.0
 )
 
 type MonitorItem struct {
@@ -18,6 +19,33 @@ type MonitorItem struct {
 	red, green, blue   float64
 	deltaFunc          func(*MonitorItem)
 	widget             *gtk.DrawingArea
+}
+
+func buildLineItem(grid *gtk.Grid, left, right *MonitorItem) {
+	mid, err := gtk.DrawingAreaNew()
+	if err != nil {
+		log.Fatal("", err)
+	}
+	mid.Connect("draw", func(da *gtk.DrawingArea, cr *cairo.Context) {
+		cr.SetSourceRGB(0, 200, 0)
+		cr.Rectangle(width/2-2, 0, lineWeight+2, lineWeight)
+		cr.Fill()
+	})
+	grid.Attach(mid, 0, 0, width, height)
+
+	grid.Attach(left.buildItem(), 0, 0, width, height)
+	grid.Attach(right.buildItem(), 0, 0, width, height)
+
+	background, err := gtk.DrawingAreaNew()
+	if err != nil {
+		log.Fatal("", err)
+	}
+	background.Connect("draw", func(da *gtk.DrawingArea, cr *cairo.Context) {
+		cr.SetSourceRGB(200, 200, 200)
+		cr.Rectangle(0, 0, lineWeight+width, lineWeight)
+		cr.Fill()
+	})
+	grid.Attach(background, 0, 0, width, height)
 }
 
 // https://docs.gtk.org/gtk3/class.DrawingArea.html
@@ -47,14 +75,24 @@ func swapMemoryInfo(t *MonitorItem) {
 	t.initX = width/2 + (100-memInfo.UsedPercent)*width/200
 	t.initX = float64(int64(t.initX))
 	t.x = width - t.initX
-	fmt.Println(t.initX, t.x)
+	// fmt.Println(t.initX, t.x)
 }
 
 func refreshDrawArea(items []*MonitorItem) {
-	for range time.NewTicker(time.Second * refreshSec).C {
-		for _, i := range items {
-			i.deltaFunc(i)
-		}
-		win.QueueDraw()
+	refreshOnce(items)
+	glib.TimeoutAdd(uint((time.Second * refreshSec).Milliseconds()), func() bool {
+		refreshOnce(items)
+		return true
+	})
+
+	//for range time.NewTicker(time.Second * refreshSec).C {
+	//	refreshOnce(items)
+	//}
+}
+
+func refreshOnce(items []*MonitorItem) {
+	for _, i := range items {
+		i.deltaFunc(i)
 	}
+	win.QueueDraw()
 }
