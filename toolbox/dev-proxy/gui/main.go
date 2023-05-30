fe831181e4081e112a220571cb086e80618b4694
kuangcp <kuangcp@aliyun.com>
2023-05-30 10:14:50 +0800

    fix

--- toolbox/dev-proxy/gui/main.go
+++ toolbox/dev-proxy/gui/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"github.com/getlantern/systray"
 	"github.com/kuangcp/gobase/toolbox/dev-proxy/core"
-	"github.com/kuangcp/gobase/toolbox/dev-proxy/app"
 )
 
 func main() {
 	core.HelpInfo.Parse()
 	core.InitConfig()
 
+	core.InitConnection()
+	defer core.CloseConnection()
+
 	// TODO 替换proxy实现
-	go app.HttpsProxy()
+	//go app.HttpsProxy()
 
 	go core.StartQueryServer()
 	go core.StartMainServer()
