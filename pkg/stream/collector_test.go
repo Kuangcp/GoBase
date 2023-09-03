9fbce01091f09f9f4928c60d898576bda0627290
Kuangcp <kuangcp@aliyun.com>
2023-09-03 22:52:43 +0800

    stream: to map

--- pkg/stream/collector_test.go
+++ pkg/stream/collector_test.go
@@ -6,16 +6,39 @@ import (
 )
 
 func TestCollectList(t *testing.T) {
-	result := ToListSelf[User](Just(1, 8, 10, 11, 20, 21).Map(func(item any) any {
-		v := item.(int)
-		return User{
-			id:     v,
-			name:   fmt.Sprint(v),
-			areaId: v / 3,
-		}
-	}))
+	result := ToList[User](
+		Just(1, 8, 10, 11, 20, 21).
+			Map(func(item any) any {
+				v := item.(int)
+				return User{
+					id:     v,
+					name:   fmt.Sprint(v),
+					areaId: v / 3,
+				}
+			}),
+	)
 	fmt.Println(result)
 	for _, u := range result {
 		fmt.Println(u.id, u.areaId)
+	}
+}
+
+func TestCollectToMap(t *testing.T) {
+	userMap := ToMap[string, User](
+		Just(1, 8, 10, 11, 20, 21).Map(func(item any) any {
+			v := item.(int)
+			return User{
+				id:     v,
+				name:   fmt.Sprint(v),
+				areaId: v / 3,
+			}
+		}),
+		func(a any) string {
+			u := a.(User)
+			return u.name
+		}, Self[User](),
+	)
+	for k, v := range userMap {
+		fmt.Println(k, v)
 	}
 }
