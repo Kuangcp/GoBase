73dfdff2c84c02f72a4ca8dc0764342cf8ed27da
Kuangcp <kuangcp@aliyun.com>
2023-09-03 22:52:43 +0800

    stream: to map

--- pkg/stream/collector.go
+++ pkg/stream/collector.go
@@ -1,10 +1,24 @@
 package stream
 
-func ToListSelf[R any](s Stream) []R {
-	return ToList[R](s, nil)
+func Self[R any]() func(any) R {
+	return func(a any) R {
+		return a.(R)
+	}
 }
 
-func ToList[R any](s Stream, fn func(s any) R) []R {
+func ToList[R any](s Stream) []R {
+	return ToListFunc[R](s, nil)
+}
+
+func ToMap[K comparable, V any](s Stream, key func(any) K, val func(any) V) map[K]V {
+	result := make(map[K]V)
+	for item := range s.source {
+		result[key(item)] = val(item)
+	}
+	return result
+}
+
+func ToListFunc[R any](s Stream, fn func(s any) R) []R {
 	var result []R
 	for item := range s.source {
 		if fn == nil {
