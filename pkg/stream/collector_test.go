ba20657eaaccb08b291904bc8893e84368600297
Kuangcp <kuangcp@aliyun.com>
2023-09-03 22:42:52 +0800

    stream: simplifiy

--- pkg/stream/collector_test.go
+++ pkg/stream/collector_test.go
@@ -6,18 +6,16 @@ import (
 )
 
 func TestCollectList(t *testing.T) {
-	result := Just(1, 8, 10, 11, 20, 21).Map(func(item any) any {
+	result := ToListSelf[User](Just(1, 8, 10, 11, 20, 21).Map(func(item any) any {
 		v := item.(int)
 		return User{
 			id:     v,
 			name:   fmt.Sprint(v),
 			areaId: v / 3,
 		}
-	}).Collect(ToListSelf())
+	}))
 	fmt.Println(result)
-	ls := result.([]any)
-	for _, i := range ls {
-		u := i.(User)
+	for _, u := range result {
 		fmt.Println(u.id, u.areaId)
 	}
 }
