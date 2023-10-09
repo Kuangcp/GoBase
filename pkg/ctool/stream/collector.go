377127115d870387a8dab9733dc0be43d95ab155
Kcp <kuangcp@aliyun.com>
2023-10-09 13:42:35 +0800

    stream: toSet

--- pkg/ctool/stream/collector.go
+++ pkg/ctool/stream/collector.go
@@ -2,19 +2,33 @@ package stream
 
 import (
 	"fmt"
+	"github.com/kuangcp/gobase/pkg/ctool"
 	"reflect"
 )
 
-func ToList[R any](s Stream) []R {
-	return ToListFunc[R](s, nil)
+var (
+	toStringType = ctool.NewSet(reflect.Uint, reflect.Int, reflect.Uint8, reflect.Int8, reflect.Uint16, reflect.Int16,
+		reflect.Uint32, reflect.Int32, reflect.Uint64, reflect.Int64)
+)
+
+func ToSet[R comparable](s Stream) *ctool.Set[R] {
+	return ToSetFunc[R](s, nil)
 }
 
-func ToMap[K comparable, V any](s Stream, key func(any) K, val func(any) V) map[K]V {
-	result := make(map[K]V)
+func ToSetFunc[R comparable](s Stream, fn func(s any) R) *ctool.Set[R] {
+	result := ctool.NewSet[R]()
 	for item := range s.source {
-		result[key(item)] = val(item)
+		if fn == nil {
+			result.Add(item.(R))
+		} else {
+			result.Add(fn(item))
+		}
 	}
 	return result
+}
+
+func ToList[R any](s Stream) []R {
+	return ToListFunc[R](s, nil)
 }
 
 func ToListFunc[R any](s Stream, fn func(s any) R) []R {
@@ -29,6 +43,18 @@ func ToListFunc[R any](s Stream, fn func(s any) R) []R {
 	return result
 }
 
+func ToMap[K comparable, V any](s Stream, key func(any) K, val func(any) V) map[K]V {
+	result := make(map[K]V)
+	for item := range s.source {
+		result[key(item)] = val(item)
+	}
+	return result
+}
+
+func ToJoin(s Stream) string {
+	return ToJoins(s, "")
+}
+
 func ToJoins(s Stream, split string) string {
 	result := ""
 	nonString := false
@@ -41,30 +67,12 @@ func ToJoins(s Stream, split string) string {
 			}
 			first = false
 			result += item.(string)
-		} else if iType.Kind() == reflect.Int || iType.Kind() == reflect.Int8 || iType.Kind() == reflect.Int16 ||
-			iType.Kind() == reflect.Int32 || iType.Kind() == reflect.Int64 {
+		} else if toStringType.Contains(iType.Kind()) {
 			if !first {
 				result += split
 			}
 			first = false
 			result += fmt.Sprint(item)
-		} else {
-			nonString = true
-		}
-	}
-	if nonString {
-		fmt.Println("warn: has no string type item")
-	}
-	return result
-}
-
-func ToJoin(s Stream) string {
-	result := ""
-	nonString := false
-	for item := range s.source {
-		iType := reflect.TypeOf(item)
-		if iType.Kind() == reflect.String {
-			result += item.(string)
 		} else {
 			nonString = true
 		}
