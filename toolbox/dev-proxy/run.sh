4b9817e637af891d92ed0867762e7fc8e029c9b4
kuangcp <kuangcp@aliyun.com>
2023-10-19 17:34:06 +0800

    proxy: shell opt

--- toolbox/dev-proxy/run.sh
+++ toolbox/dev-proxy/run.sh
@@ -8,25 +8,40 @@ white='\033[0;37m'
 end='\033[0m'
 grey='\033[3;37;40m'
 
-total=4
-i=0
 log_info(){
     i=$(($i + 1))
-    printf "$green $i/$total $blue $1 $end\n"
+    ts=$(date "+%F %T.%N" | cut -b 1-23)
+    printf "$green$ts $i/$total $blue $1 $end\n"
 }
 
-last_commit=$(git log --oneline | head -n 1 | awk '{print $1}')
-dt=$(date +%m%d%H%M)
+build_image(){
+    total=4
+    i=0
+    last_commit=$(git log --oneline | head -n 1 | awk '{print $1}')
+    dt=$(date +%m%d%H%M)
 
-log_info 'Delete exist container'
-docker stop dev-proxy
-docker rm dev-proxy
+    log_info 'Stop exist container'
+    docker stop dev-proxy
 
-log_info 'Build image'
-# yay docker-buildx
-docker buildx build -t dev-proxy:1.0-$dt-$last_commit .
+    log_info 'Delete container'
+    docker rm dev-proxy
 
-log_info 'Start new container'
-docker run --init -d --name dev-proxy --network host -v $HOME/Apps/dev-proxy-cd:/root/.dev-proxy/ dev-proxy:1.0-$dt-$last_commit
+    log_info 'Build image'
+    # yay docker-buildx
+    docker buildx build -t dev-proxy:1.0-$dt-$last_commit .
 
-log_info 'Finish rebuild'
+    log_info 'Start new container'
+    docker run --init -d --name dev-proxy --network host -v $HOME/Apps/dev-proxy-cd:/root/.dev-proxy/ dev-proxy:1.0-$dt-$last_commit
+
+    log_info 'Finish rebuild'
+}
+
+case $1 in 
+clean)
+
+;;
+
+*)
+   build_image
+;;
+esac
