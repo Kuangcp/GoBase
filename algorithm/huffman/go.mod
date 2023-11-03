47dac48a790d0ebb408e2a2a07197919e1a9e7af
kuangcp <kuangcp@aliyun.com>
2023-11-03 19:24:33 +0800

    move

--- algorithm/huffman/go.mod
+++ algorithm/huffman/go.mod
@@ -1,5 +1,3 @@
 module huffman
 
-go 1.14
-
-require github.com/theodesp/go-heaps v0.0.0-20190520121037-88e35354fe0a
+go 1.20
--- algorithm/huffman/tree.go
+++ algorithm/huffman/tree.go
@@ -1,6 +1,9 @@
 package huffman
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type (
 	Node struct {
@@ -16,14 +19,40 @@ type (
 	}
 )
 
-func NewHuffmanTree(formatStr string) HuffmanTree {
-	tree := HuffmanTree{Len: len(formatStr)}
+func NewHuffmanTree(formatStr string) *HuffmanTree {
+	type cc struct {
+		char  uint8
+		count int
+	}
+
+	cache := make(map[uint8]*cc)
+	for i := range formatStr {
+		char := formatStr[i]
+		_, ok := cache[char]
+		if !ok {
+			cache[char] = &cc{char: char, count: 1}
+		} else {
+			cache[char].count++
+		}
+	}
+
+	var l []*cc
+	for _, v := range cache {
+		l = append(l, v)
+	}
+
+	sort.Slice(l, func(i, j int) bool {
+		return l[i].count < l[j].count
+	})
+
+	tree := &HuffmanTree{Len: len(l)}
 	rootPtr := &Node{
 		Code: 0,
 	}
+	rootPtr = nil
 
-	for i := range formatStr {
-		char := formatStr[i]
+	for _, i := range l {
+		char := i.char
 		if rootPtr == nil {
 			tree.Root = &Node{
 				Char: char,
@@ -55,21 +84,22 @@ func (t *HuffmanTree) print() {
 	if t.Root == nil {
 		return
 	}
-	var ptr = t.Root
-	fmt.Println(string(t.Root.Char), t.Root.Code)
-	for {
-		if ptr.Left == nil {
-			break
-		} else {
-			fmt.Println(string(ptr.Left.Char), ptr.Left.Code)
-		}
-		if ptr.Right != nil {
-			fmt.Println(string(ptr.Right.Char), ptr.Right.Code)
-			ptr = ptr.Left
-		}
+	printNode(t.Root, "")
+}
+func printNode(n *Node, path string) {
+	if n == nil {
+		return
+	}
+	curPath := fmt.Sprintf("%v%v", path, n.Code)
+	fmt.Println(string(n.Char), curPath)
+	if n.Left != nil {
+		printNode(n.Left, curPath)
+	}
+	if n.Right != nil {
+		printNode(n.Right, curPath)
 	}
 }
 
-func Build(){
+func Build() {
 
 }
