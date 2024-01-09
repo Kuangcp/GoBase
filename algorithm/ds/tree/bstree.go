fa2a8da59443145c8a85dd395e80a0a87f90f3b8
kuangcp <kuangcp@aliyun.com>
2024-01-09 15:29:33 +0800

    rank

--- algorithm/ds/tree/bstree.go
+++ algorithm/ds/tree/bstree.go
@@ -19,7 +19,14 @@ type (
 func InitBsTree[T ctool.Numberic]() *BsTree[T] {
 	return &BsTree[T]{}
 }
-
+func (b *BsTree[T]) Inserts(val ...T) {
+	if len(val) == 0 {
+		return
+	}
+	for _, v := range val {
+		b.Insert(v)
+	}
+}
 func (b *BsTree[T]) Insert(val T) {
 	if b.Root == nil {
 		b.Root = newNode(val)
@@ -28,12 +35,74 @@ func (b *BsTree[T]) Insert(val T) {
 
 	insert(b.Root, val)
 }
-func (b *BsTree[T]) Delete(val T) {
-	delete(b.Root, val)
 
+func (b *BsTree[T]) Delete(val T) {
+	b.Root = removeNode(b.Root, val)
 }
+
+func removeNode[T ctool.Numberic](b *BsNode[T], val T) *BsNode[T] {
+	if b == nil {
+		return nil
+	}
+
+	if val < b.Data {
+		b.Left = removeNode(b.Left, val)
+	} else if val > b.Data {
+		b.Right = removeNode(b.Right, val)
+	} else {
+		if b.Count > 1 {
+			b.Count -= 1
+			return b
+		} else {
+			if b.Left == nil {
+				return b.Right
+			} else if b.Right == nil {
+				return b.Left
+			} else {
+				// 重新选 左树最大值或者右树最小值作为中间值
+				p := b.Left
+				for p != nil {
+					temp := p.Right
+					if temp == nil {
+						break
+					}
+					p = p.Right
+				}
+				b.Data = p.Data
+				b.Count = p.Count
+				b.Left = removeNode(b.Left, p.Data)
+			}
+		}
+	}
+	return b
+}
+
 func (b *BsTree[T]) Search(val T) *BsNode[T] {
 	return search(b.Root, val)
+}
+
+func (b *BsTree[T]) Rank(val T) int {
+	return rank(b.Root, val) + 1
+}
+
+func rank[T ctool.Numberic](node *BsNode[T], val T) int {
+	if node == nil {
+		return 0
+	}
+	if node.Data == val {
+		return sumCount(node.Left)
+	} else if val < node.Data {
+		return rank(node.Left, val)
+	} else {
+		return sumCount(node.Left) + rank(node.Right, val) + node.Count
+	}
+}
+
+func sumCount[T ctool.Numberic](node *BsNode[T]) int {
+	if node == nil {
+		return 0
+	}
+	return sumCount(node.Left) + sumCount(node.Right) + node.Count
 }
 
 func (b *BsTree[T]) Min() T {
@@ -64,25 +133,6 @@ func (b *BsTree[T]) Max() T {
 
 func newNode[T ctool.Numberic](val T) *BsNode[T] {
 	return &BsNode[T]{Data: val, Count: 1}
-}
-
-func delete[T ctool.Numberic](b *BsNode[T], val T) *BsNode[T] {
-	if b == nil {
-		return nil
-	}
-	if b.Data == val {
-		if b.Count > 1 {
-			b.Count -= 1
-			return nil
-		} else {
-			return b
-		}
-	}
-	if val < b.Data {
-		return delete(b.Left, val)
-	} else {
-		return delete(b.Right, val)
-	}
 }
 
 func search[T ctool.Numberic](b *BsNode[T], val T) *BsNode[T] {
