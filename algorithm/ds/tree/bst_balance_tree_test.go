4df8880645e4f23788286e41c058f5caab76db98
Kuangcp <kuangcp@aliyun.com>
2024-01-09 23:14:28 +0800

    fix: each node balance

--- algorithm/ds/tree/bst_balance_tree_test.go
+++ algorithm/ds/tree/bst_balance_tree_test.go
@@ -16,7 +16,8 @@ func TestInitBsBalance(t *testing.T) {
 
 func TestBalance1(t *testing.T) {
 	tree := InitBsBalanceTree[int]()
-	tree.Inserts(4, 9, 7, 22, 24, 25, 40, 10, 18, 8, 5, 30, 10, 11, 15, 99, 6, 24, 23, 1, 27, 55, 3, 12, 13, 21, 14, 16, 17, 2, 26, 20)
+	tree.Inserts(7, 6, 1, 2, 8, 10, 12, 99, 30, 20, 23)
+	//tree.Inserts(77, 56, 39, 42, 4, 86, 9, 7, 22, 83, 24, 25, 40, 10, 18, 8, 5, 30, 87, 19, 28, 29, 10, 11, 15, 99, 6, 24, 23, 88, 1, 27, 55, 3, 12, 13, 21, 14, 45, 48, 49, 50, 66, 81, 82, 16, 17, 2, 26, 20, 41)
 
 	fmt.Println(algo.PrintBiMindMap(tree.Root))
 	// 调试方式 安装plantuml插件后分屏到右侧，修改上诉序列可实现实时查看树调整情况
@@ -24,6 +25,7 @@ func TestBalance1(t *testing.T) {
 	algo.WriteBiMindMap(tree.Root, "init-balance.puml")
 }
 
+// TODO https://blog.csdn.net/boysoft2002/article/details/126908846 树绘制成标准svg图 或者直接2D绘制窗口
 func TestBstBalanceDfsPre(t *testing.T) {
 
 }
