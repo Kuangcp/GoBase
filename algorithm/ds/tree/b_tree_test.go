1e3acb3c0e6c747178c23a8ca6eead690ed5276a
Kcp <kuangcp@aliyun.com>
2024-02-16 21:41:21 +0800

    wip: record index

--- algorithm/ds/tree/b_tree_test.go
+++ algorithm/ds/tree/b_tree_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/kuangcp/gobase/pkg/ctool"
 	"github.com/kuangcp/gobase/pkg/ctool/algo"
 	"math"
 	"testing"
@@ -19,6 +20,24 @@ func TestInitBayerTree(t *testing.T) {
 	//tree.Insert(5, "5key")
 	//tree.Insert(2, "2key")
 	tree.Insert(3, "3-2key")
+
+	println(algo.PrintNMindMap(tree.root))
+}
+
+// 模拟数据库中存储行数据的索引
+func TestInitRecordBayerTree(t *testing.T) {
+	type User struct {
+		id     int
+		name   string
+		addr   string
+		job    string
+		school string
+	}
+
+	tree := CreateBayerTree[int, User](5)
+
+	tree.Insert(3, User{id: 3, name: ctool.RandomAlpha(4)})
+	tree.Insert(3, User{id: 3, name: ctool.RandomAlpha(4)})
 
 	println(algo.PrintNMindMap(tree.root))
 }
