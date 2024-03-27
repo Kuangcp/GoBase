18cdea43686df9e5d89947a376690d040170a0b1
kuangcp <kuangcp@aliyun.com>
2024-03-27 12:30:00 +0800

    opt: reduce useless logic

--- toolbox/dev-proxy/app/requestctx.go
+++ toolbox/dev-proxy/app/requestctx.go
@@ -3,10 +3,11 @@ package app
 import "github.com/kuangcp/gobase/toolbox/dev-proxy/core"
 
 type ReqCtx struct {
-	reqLog      *core.ReqLog[core.Message]
-	proxyLog    string
-	proxyType   string
-	startReq    int64
-	receiveReq  int64
-	needStorage bool
+	reqLog       *core.ReqLog[core.Message]
+	proxyLog     string
+	proxyType    string
+	startReqMs   int64
+	receiveReqMs int64
+	needStorage  bool
+	originUrl    string
 }
