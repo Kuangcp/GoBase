bcb439d3737faa587c98e07049d2bd1cbf135970
kuangcp <kuangcp@aliyun.com>
2024-07-22 13:55:24 +0800

    wip: del header

--- toolbox/dev-proxy/web/req_service.go
+++ toolbox/dev-proxy/web/req_service.go
@@ -8,18 +8,19 @@ import (
 	"net/http"
 )
 
+type HostHeaderParam struct {
+	Host string `form:"host"`
+	Key  string `form:"key"`
+	Val  string `form:"val"`
+}
+
 func SetReqHeader(request *http.Request) ctool.ResultVO[string] {
 	bd, err := io.ReadAll(request.Body)
 	if err != nil {
 		return ctool.FailedWithMsg[string](err.Error())
 	}
 
-	type data struct {
-		Host string `form:"host"`
-		Key  string `form:"key"`
-		Val  string `form:"val"`
-	}
-	var d data
+	var d HostHeaderParam
 	json.Unmarshal(bd, &d)
 	core.SetHeader(d.Host, d.Key, d.Val)
 	return ctool.Success[string]()
@@ -31,9 +32,14 @@ func GetReqHeader(request *http.Request) ctool.ResultVO[map[string]string] {
 }
 
 func DelReqHeader(request *http.Request) ctool.ResultVO[string] {
-	query := request.URL.Query()
-	host := query.Get("host")
-	key := query.Get("key")
-	core.DeleteHeader(host, key)
+	bd, err := io.ReadAll(request.Body)
+	if err != nil {
+		return ctool.FailedWithMsg[string](err.Error())
+	}
+
+	var d HostHeaderParam
+	json.Unmarshal(bd, &d)
+
+	core.DeleteHeader(d.Host, d.Key)
 	return ctool.Success[string]()
 }
