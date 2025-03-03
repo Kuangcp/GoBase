bd0b57e45d64b174a38877ada5c37b337c7ac2e8
kuangcp <kuangcp@aliyun.com>
2025-03-03 18:18:16 +0800

    join

--- toolbox/joinstr/main.go
+++ toolbox/joinstr/main.go
@@ -4,11 +4,56 @@ import (
 	"fmt"
 	"github.com/atotto/clipboard"
 	"github.com/kuangcp/logger"
+	"github.com/kuangcp/gobase/pkg/ctool"
 	"strings"
 )
 
+
+var (
+	buildVersion string
+
+	help         bool
+	long         bool
+)
+
+var info = ctool.HelpInfo{
+	Description:   "Start static file web server on current path",
+	Version:       "1.1.0",
+	BuildVersion:  buildVersion,
+	SingleFlagLen: -2,
+	ValueLen:      -6,
+	Flags: []ctool.ParamVO{
+		{Short: "-h", BoolVar: &help, Comment: "help"},
+		{Short: "-l", BoolVar: &long, Comment: "join long"},
+	},
+	Options: []ctool.ParamVO{
+		// {Short: "-p", Value: "port", Comment: "web server port"},
+		// {Short: "-d", Value: "folder", Comment: "folder pair. like -d x=y "},
+	}}
+
 func main() {
+	info.Parse()
+	if help {
+		info.PrintHelp()
+		return
+	}
+	if long {
+		concatNumberLong()
+		return
+	}
+
 	concatNumber()
+}
+
+func concatNumberLong() {
+	lines := readClipList()
+	result := strings.Join(lines, "L,")
+	result += "L"
+	fmt.Println(result)
+	err := clipboard.WriteAll(result)
+	if err != nil {
+		logger.Fatal(err)
+	}
 }
 
 func concatNumber() {
