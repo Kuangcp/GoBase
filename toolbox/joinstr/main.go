4779ea07aaabdf27d8701d138f384c0a1605ff4f
kuangcp <kuangcp@aliyun.com>
2025-03-08 16:21:13 +0800

    join

--- toolbox/joinstr/main.go
+++ toolbox/joinstr/main.go
@@ -3,33 +3,34 @@ package main
 import (
 	"fmt"
 	"github.com/atotto/clipboard"
-	"github.com/kuangcp/logger"
 	"github.com/kuangcp/gobase/pkg/ctool"
+	"github.com/kuangcp/gobase/pkg/ctool/stream"
+	"github.com/kuangcp/logger"
 	"strings"
 )
-
 
 var (
 	buildVersion string
 
-	help         bool
-	long         bool
+	help bool
+	long bool
+	str  bool
+	strs bool
 )
 
 var info = ctool.HelpInfo{
-	Description:   "Start static file web server on current path",
-	Version:       "1.1.0",
+	Description:   "read clip , join , write clip",
+	Version:       "1.1.1",
 	BuildVersion:  buildVersion,
 	SingleFlagLen: -2,
 	ValueLen:      -6,
 	Flags: []ctool.ParamVO{
 		{Short: "-h", BoolVar: &help, Comment: "help"},
 		{Short: "-l", BoolVar: &long, Comment: "join long"},
+		{Short: "-s", BoolVar: &str, Comment: "join single quote string"},
+		{Short: "-d", BoolVar: &strs, Comment: "join double quote string"},
 	},
-	Options: []ctool.ParamVO{
-		// {Short: "-p", Value: "port", Comment: "web server port"},
-		// {Short: "-d", Value: "folder", Comment: "folder pair. like -d x=y "},
-	}}
+	Options: []ctool.ParamVO{}}
 
 func main() {
 	info.Parse()
@@ -38,27 +39,47 @@ func main() {
 		return
 	}
 	if long {
-		concatNumberLong()
+		concatStream(func(s stream.Stream) string {
+			return stream.ToJoins(
+				s.Map(func(item any) any {
+					return item.(string) + "L"
+				}), ",")
+		})
+		return
+	}
+	if str {
+		concatStream(func(s stream.Stream) string {
+			return stream.ToJoins(
+				s.Map(func(item any) any {
+					return "'" + item.(string) + "'"
+				}), ",")
+		})
+		return
+	}
+	if strs {
+		concatStream(func(s stream.Stream) string {
+			return stream.ToJoins(
+				s.Map(func(item any) any {
+					return "\"" + item.(string) + "\""
+				}), ",")
+		})
 		return
 	}
 
-	concatNumber()
+	concatStream(func(s stream.Stream) string {
+		return stream.ToJoins(s, ",")
+	})
 }
 
-func concatNumberLong() {
+func concatStream(fun func(s stream.Stream) string) {
 	lines := readClipList()
-	result := strings.Join(lines, "L,")
-	result += "L"
-	fmt.Println(result)
-	err := clipboard.WriteAll(result)
-	if err != nil {
-		logger.Fatal(err)
-	}
-}
-
-func concatNumber() {
-	lines := readClipList()
-	result := strings.Join(lines, ",")
+	from := stream.From(func(source chan<- any) {
+		for _, s := range lines {
+			source <- s
+		}
+	})
+	result := fun(from)
+	//result := stream.ToJoins(from, ",")
 	fmt.Println(result)
 	err := clipboard.WriteAll(result)
 	if err != nil {
