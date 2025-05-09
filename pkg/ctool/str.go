6bbe2b4b70acbcefa45ff644785fafd882ab7eda
kuangcp <kuangcp@aliyun.com>
2025-05-09 10:46:36 +0800

    opt

--- pkg/ctool/str.go
+++ pkg/ctool/str.go
@@ -10,38 +10,43 @@ var list = []string{"A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", 
 var alphaAndNum = []string{"A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i",
 	"J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t",
 	"U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+var alphaDict = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+var alphaDictLen = 52
+var alphanumericDict = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789"
+var alphanumericDictLen = 62
 
 func RandomAlpha(length int) string {
-	result := ""
+	bys := make([]byte, 0, length)
 	for i := 0; i < length; i++ {
-		idx := rand.Intn(len(list))
-		result += list[idx]
+		idx := rand.Intn(alphaDictLen)
+		bys = append(bys, alphaDict[idx:idx+1]...)
 	}
-
-	return result
+	return string(bys)
 }
 
 func RandomAlNum(length int) string {
-	result := ""
+	bys := make([]byte, 0, length)
 	for i := 0; i < length; i++ {
-		idx := rand.Intn(len(alphaAndNum))
-		result += alphaAndNum[idx]
+		idx := rand.Intn(alphanumericDictLen)
+		bys = append(bys, alphanumericDict[idx:idx+1]...)
 	}
 
-	return result
+	return string(bys)
 }
 
 // RandomAlNumValid 生成非数字开头的字符串，数据库等地方使用
 func RandomAlNumValid(length int) string {
-	result := ""
-	for len(result) < length {
-		idx := rand.Intn(len(alphaAndNum))
-		single := alphaAndNum[idx]
-		if len(result) == 0 && single >= "0" && single <= "9" {
+	bys := make([]byte, 0, length)
+	ls := 0
+	for ls < length {
+		idx := rand.Intn(alphanumericDictLen)
+		single := alphanumericDict[idx : idx+1]
+		if ls == 0 && single >= "0" && single <= "9" {
 			continue
 		}
-		result += single
+		bys = append(bys, single...)
+		ls++
 	}
 
-	return result
+	return string(bys)
 }
--- pkg/ctool/str_test.go
+++ pkg/ctool/str_test.go
@@ -1,31 +1,49 @@
 package ctool
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
+func watchNs(title string, act func()) {
+	start := time.Now().UnixMilli()
+	act()
+	end := time.Now().UnixMilli()
+	fmt.Printf("%s %d ms\n", title, end-start)
+}
+
 func TestStr(t *testing.T) {
 	println(RandomAlpha(10))
 
-	langs := NewSet[string]()
+	time.Sleep(time.Nanosecond * 20)
+	l := 7
+	loop := 1000000
+	watchNs("one", func() {
+		langs := NewSet[string]()
+		for i := 0; i < loop; i++ {
+			langs.Add(RandomAlpha(l))
+		}
+		println(langs.Len())
+	})
+}
 
+func TestNum(t *testing.T) {
+	RandomAlNum(8)
+
+	langs := NewSet[string]()
 	time.Sleep(time.Nanosecond * 20)
 	for i := 0; i < 10000; i++ {
-		langs.Add(RandomAlpha(4))
+		langs.Add(RandomAlNum(4))
 	}
 	println(langs.Len())
 }
 
-func TestNum(t *testing.T) {
-	for i := 0; i < 10; i++ {
-		println(RandomAlNum(8))
-	}
-}
-
 func TestNameValid(t *testing.T) {
+	langs := NewSet[string]()
 	for i := 0; i < 10000; i++ {
 		valid := RandomAlNumValid(5)
-		println(valid)
+		langs.Add(valid)
 	}
+	println(langs.Len())
 }
