50b6c11b4edde7d8e79acd89d1de57def85e6a58
Kuangcp <kuangcp@aliyun.com>
2026-06-26 17:48:53 +0800

    remove killer, fix leak

--- toolbox/keylogger/gui/main.go
+++ toolbox/keylogger/gui/main.go
@@ -7,14 +7,9 @@ import (
 	"github.com/gotk3/gotk3/cairo"
 	"github.com/kuangcp/gobase/pkg/ctool"
 	"github.com/kuangcp/gobase/toolbox/keylogger/app/store"
-	"github.com/shirou/gopsutil/mem"
 	"log"
 	"math"
-	"sort"
-	"strconv"
-	"strings"
 	"time"
-	"unsafe"
 
 	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/glib"
@@ -98,11 +93,6 @@ func main() {
 	//main2()
 	//go notifyAny()
 
-	// killer
-	var list = []string{"firefox"}
-	go runKiller(list)
-	//go oomKiller()
-
 	option = redis.Options{Addr: host + ":" + port, Password: pwd, DB: db}
 	store.InitConnection(option, false)
 
@@ -121,72 +111,7 @@ func main() {
 	//gtk.Main()
 }
 
-func oomKiller() {
-	type Score struct {
-		pid   string
-		score int
-	}
-	for range time.NewTicker(time.Second * 10).C {
-		memInfo, _ := mem.VirtualMemory()
-		//fmt.Println(memInfo.UsedPercent)
-		if memInfo.UsedPercent < 95 {
-			continue
-		}
-		var list []Score
-		out, _ := execCommand("ls /proc/*/oom_score")
-		pids := strings.Split(out, "\n")
-		for _, pid := range pids {
-			if strings.Contains(pid, "self") || pid == "" {
-				continue
-			}
-			oomScore, success := execCommand("cat " + pid)
-			if !success {
-				continue
-			}
 
-			id := strings.Split(pid, "/")[2]
-			//fmt.Printf("%v  %v [%v] \n", id, pid, strings.TrimSpace(oomScore))
-			score, _ := strconv.Atoi(strings.TrimSpace(oomScore))
-			if score == 0 {
-				continue
-			}
-			list = append(list, Score{pid: id, score: score})
-		}
-		sort.Slice(list, func(i, j int) bool {
-			return list[i].score >= list[j].score
-		})
-		if len(list) == 0 {
-			continue
-		}
-		logger.Error("all: ", len(list), " kill PID:", list[0].pid, list[0].score)
-		execCommand("kill -9" + list[0].pid)
-	}
-
-}
-
-func runKiller(list []string) {
-	for range time.NewTicker(time.Second * 5).C {
-		memInfo, _ := mem.VirtualMemory()
-		//fmt.Println(memInfo.UsedPercent)
-		if memInfo.UsedPercent < 92 {
-			continue
-		}
-		for _, app := range list {
-			command, _ := execCommand("ps ux | awk '/" + app + "/ && !/awk/ {print $2}'")
-			if command == "" {
-				continue
-			}
-			pids := strings.Split(command, "\n")
-			for _, id := range pids {
-				if id == "" {
-					continue
-				}
-				logger.Warn("kill " + id)
-				execCommand("kill " + id)
-			}
-		}
-	}
-}
 
 func createWindow() {
 	grid := createMainGrid()
@@ -301,28 +226,22 @@ func createMainGrid() *gtk.Widget {
 }
 
 func bindMouseActionForWindow() {
-	// 鼠标最后点击坐标
 	var x, y int
 	win.SetEvents(int(gdk.BUTTON_PRESS_MASK | gdk.BUTTON1_MOTION_MASK))
 
-	//鼠标按下事件处理
 	win.Connect("button-press-event", func(widget *gtk.Window, ctx *gdk.Event) {
-		//获取鼠键按下属性结构体变量，系统内部的变量，不是用户传参变量
-		event := *(*gdk.EventButton)(unsafe.Pointer(&ctx))
-		if event.Button() == 1 { //左键
-			x, y = int(event.X()), int(event.Y()) //保存点击的起点坐标
-		} else if event.Button() == 3 { //右键
-			//右键，关闭窗口
+		event := gdk.EventButtonNewFromEvent(ctx)
+		if event.Button() == 1 {
+			x, y = int(event.X()), int(event.Y())
+		} else if event.Button() == 3 {
 			app.Quit()
 		}
 	})
 
-	// 鼠标移动事件处理
-	// 注意： 不出现偏差的前提是 应用 内不出现其他点击事件和交互
 	win.Connect("motion-notify-event", func(widget *gtk.Window, ctx *gdk.Event) {
-		//获取鼠标移动属性结构体变量，系统内部的变量，不是用户传参变量
-		event := *(*gdk.EventButton)(unsafe.Pointer(&ctx))
-		win.Move(int(event.XRoot())-x, int(event.YRoot())-y)
+		event := gdk.EventMotionNewFromEvent(ctx)
+		xRoot, yRoot := event.MotionValRoot()
+		win.Move(int(xRoot)-x, int(yRoot)-y)
 	})
 }
 
@@ -354,8 +273,12 @@ func latestLabelStr(now time.Time) string {
 
 // 从缓存中更新窗口内面板
 func timeoutRefresh(period time.Duration) {
-	// 返回 true 才能一直执行
 	glib.TimeoutAdd(uint(period.Milliseconds()), func() bool {
+		defer func() {
+			if r := recover(); r != nil {
+				logger.Error("timeoutRefresh panic: %v", r)
+			}
+		}()
 		str := latestLabelStr(time.Now())
 		kpmLabel.SetMarkup(str)
 		return true
